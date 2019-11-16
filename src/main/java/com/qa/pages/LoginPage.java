f966b2e54567dc97773b77f42ac149292bc34111
Poonam Wanare <poonamshekokar15@gmail.com>
2019-11-16 17:55:08 +0530

    maven build

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,7 +1,6 @@
 package com.qa.pages;
 
 import java.io.FileNotFoundException;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
